e3203b5672994177a7c18972df7b7c9a40591dc7
jojos <karanikasg@gmail.com>
2019-09-22 23:27:23 +0200

    add LocalDate formatter and parser examples

--- src/main/java/com/jojos/home/datetime/Parser.java
+++ src/main/java/com/jojos/home/datetime/Parser.java
@@ -1,6 +1,7 @@
 package com.jojos.home.datetime;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
@@ -10,6 +11,8 @@ import java.util.Date;
  */
 public class Parser {
     private static final DateTimeFormatter POTENTIAL_FORMATS = DateTimeFormatter.ofPattern("[HH:mm:ss][HH:mm]");
+    private static final DateTimeFormatter POTENTIAL_DATE_FORMATS = DateTimeFormatter.ofPattern("[yyyy_MM_dd][yyyy-MM-dd]");
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("[yyyy_MM_dd]");
 
     /**
      * This function attempts to parse a time. Supporting the use of potentially multiple formats from a single place.
@@ -17,9 +20,18 @@ public class Parser {
      * @param string the time
      * @return a LocalTIme object
      */
-    private static LocalTime parseLocalTime(String string) {
+    public static LocalTime parseLocalTime(String string) {
         return LocalTime.parse(string, POTENTIAL_FORMATS);
     }
+
+    public static String formatLocalDate(LocalDate localDate) {
+        return localDate.format(DATE_FORMAT);
+    }
+
+    public static LocalDate parseLocalDate(String string) {
+        return LocalDate.parse(string, POTENTIAL_DATE_FORMATS);
+    }
+
 
     public static String getTodaysDate() {
         Date date = new Date();
